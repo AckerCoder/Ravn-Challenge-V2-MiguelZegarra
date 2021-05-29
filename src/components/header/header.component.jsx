bbc0722535b8a0f76d047008671f214d4b8341d1
ackercoder <ackercoder@gmail.com>
2021-05-28 21:20:40 -0500

    Setting the project structure

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './header.styles.scss';
+
+const Header = () => {
+    return(
+        <div className="header">
+            <h3>Ravn Star Wars Registry</h3>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/pages/home-page/home-page.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../../components/header/header.component';
+
+import './home-page.styles.scss';
+
+const HomePage = () => {
+    return(
+        <div className="home-page">
+            <Header/>
+        </div>
+    )
+}
+
+export default HomePage;
